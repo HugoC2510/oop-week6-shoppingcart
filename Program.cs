05b8d6d57dbffe66ff3167ca60c1ad85b448177b
HugoC2510 <71280924+HugoC2510@users.noreply.github.com>
2020-10-19 16:51:37 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -11,16 +11,27 @@ namespace _23168_Hugo_Camps_OOP_Week6
         static void Main(string[] args)
         {
             List<Fruit> bag = new List<Fruit>();
-            Orange O1 = new Orange();
-            Apple A1 = new Apple();
+            Orange O1 = new Orange(); Orange O2 = new Orange();
+            Apple A1 = new Apple(); Apple A2 = new Apple();
             double cost = 0;
-            bag.Add(O1);bag.Add(A1);
+            bag.Add(O1);bag.Add(A1); bag.Add(A2); bag.Add(O2);
             foreach (Fruit item in bag) 
             {
-                cost = cost + item.price; 
-            } 
-            
-            Console.WriteLine("the price is "+ cost);
+
+                if(item is Orange)
+                {
+                    cost = cost + 0.60;
+                    Console.Write("Apple ");
+                }
+                if (item is Apple)
+                {
+                    cost = cost + 0.25;
+                    Console.Write("Orange ");
+                }
+       
+            }
+            Console.WriteLine();
+            Console.WriteLine("the price is "+ cost +"E");
             Console.Read();
         }
     }
